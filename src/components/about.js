cb24f551dc07e0bb2397d7d593304618f1501f08
adeha047 <dehaven7777@gmail.com>
2021-05-11 18:41:28 -0400

    added new files

--- src/components/about.js
+++ src/components/about.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Drew from "./Drew.jpg";
 
 class About extends Component {
   render() {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
+      // var profilepic= "images/" +this.props.data.image;
+      var profilepic= "Drew.jpg";
       var bio = this.props.data.bio;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
@@ -19,7 +21,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Drew's photo" />
+            <img className="profile-pic"  src={Drew} alt="Drew's photo" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
